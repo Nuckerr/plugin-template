2fc1e91db49ef4bfeb42d879f5998476d570eabd
Nucker <nuckermail@gmail.com>
2022-04-16 12:22:30 +0100

    Initial Commit

---
+++ build.gradle.kts
@@ -0,0 +1,46 @@
+import net.minecrell.pluginyml.bukkit.BukkitPluginDescription
+
+plugins {
+    java
+    id("xyz.jpenilla.run-paper") version "1.0.6"
+    id("net.minecrell.plugin-yml.bukkit") version "0.5.1"
+    id("com.github.johnrengelman.shadow") version "7.0.0"
+}
+
+group = "wtf.nucker"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    maven(url = "https://repo.codemc.io/repository/nms/")
+    maven(url = "https://jitpack.io")
+}
+
+dependencies {
+    compileOnly("org.spigotmc:spigot:1.8.8-R0.1-SNAPSHOT")
+    implementation("com.github.simplix-softworks:simplixstorage:3.2.4")
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
+
+    runServer {
+        minecraftVersion("1.8.8")
+    }
+}
+
+
+bukkit {
+    load = BukkitPluginDescription.PluginLoadOrder.STARTUP
+    main = "wtf.nucker.pluginname.PluginName"
+    apiVersion = "1.8"
+
+    name = getName()
+    description = getDescription()
+    version = getVersion().toString()
+    authors = listOf("Nucker")
+    website = "nucker.me"
+    prefix = getName()
+}
